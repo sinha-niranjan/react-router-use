218ca68cac0d037ae272b25c3b9d7bf6871615f2
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-01-03 14:16:31 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+ 
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-green-600'>
+        React Router
+      </div>
+         
+    </>
+  )
+}
+
+export default App
